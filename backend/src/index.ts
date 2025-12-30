fc93097494152c168f39aaa2d7a3b82ee3e6e45b
Rishabh1-dubey <rishabhdubey006@gmail.com>
2025-12-30 21:59:41 +0530

    make some change on package.json file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import app from "../src/app";
+import app from "./app";
+
+// Add this block to allow local execution
+const PORT = process.env.PORT || 8000;
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
 export default app;

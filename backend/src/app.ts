849266fbd5d0d48e458b05521b4fff6e8957b95e
Rishabh1-dubey <rishabhdubey006@gmail.com>
2025-12-30 10:23:53 +0530

    ready for dep;oyment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./db";
-import chatRoutes from "../src/routes/chat.route";
+import chatRoutes from "./routes/chat.route";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// IMPORTANT: connect DB, but DO NOT listen
 connectDB();
-app.use("/chat", chatRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.use("/chat", chatRoutes);
 
 export default app;
